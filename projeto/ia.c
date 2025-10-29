afbc7f0c705be6f23664d9ef7a76b5b0f44897cf
mhenriquelima <mhenriquedelima1@gmail.com>
2025-10-29 09:18:12 -0300

    feat: add pontuacao para os movimentos da ia para ela escolher o melhor no momento

--- projeto/ia.c
+++ projeto/ia.c
@@ -7,6 +7,9 @@ void jogarIA(Jogo *jogo, Jogador *jogadorIA, Jogador *jogadorOponente) {
     typedef struct {
         int linhaOrigem, colunaOrigem;
         int linhaDestino, colunaDestino;
+        int dama;
+        int captura;
+        int pontuacao;
     } Movimento;
 
     Movimento movimentosValidos[200];
@@ -15,81 +18,147 @@ void jogarIA(Jogo *jogo, Jogador *jogadorIA, Jogador *jogadorOponente) {
     for (int linha = 0; linha < MAX; linha++) {
         for (int coluna = 0; coluna < MAX; coluna++) {
             TipoPeca peca = jogo->tabuleiro[linha][coluna].peca;
-
             if (!pecaPreta(peca)) continue;
 
-            int direcoes[4][2] = {
-                {1, 1}, {1, -1}, {-1, 1}, {-1, -1}
-            };
+            int ehDama = pecaDama(peca);
+            int direcoes[4][2] = { {1, -1}, {1, 1}, {-1, -1}, {-1, 1} };
 
-            for (int d = 0; d < 4; d++) {
-                int dl = direcoes[d][0];
-                int dc = direcoes[d][1];
+            for (int d = 0; d < (ehDama ? 4 : 2); d++) {
+                int dirLinha = direcoes[d][0];
+                int dirColuna = direcoes[d][1];
+                
+                int novaLinha = linha + dirLinha;
+                int novaColuna = coluna + dirColuna;
 
-                int novaLinha = linha + dl;
-                int novaColuna = coluna + dc;
-
-                if (posicaoValida(novaLinha, novaColuna) &&
-                    jogo->tabuleiro[novaLinha][novaColuna].peca == VAZIO &&
-                    (!pecaDama(peca) ? dl == 1 : 1)) {
-                    movimentosValidos[numMovimentos++] = (Movimento){linha, coluna, novaLinha, novaColuna};
+                if (posicaoValida(novaLinha, novaColuna) && jogo->tabuleiro[novaLinha][novaColuna].peca == VAZIO) {
+                    movimentosValidos[numMovimentos++] = (Movimento){linha, coluna, novaLinha, novaColuna, ehDama, 0, 1};
                 }
 
-                int captLinha = linha + 2 * dl;
-                int captColuna = coluna + 2 * dc;
-                int meioLinha = linha + dl;
-                int meioColuna = coluna + dc;
+                int captLinha = linha + 2 * dirLinha;
+                int captColuna = coluna + 2 * dirColuna;
+                int meioLinha = linha + dirLinha;
+                int meioColuna = coluna + dirColuna;
 
                 if (posicaoValida(captLinha, captColuna) &&
                     jogo->tabuleiro[captLinha][captColuna].peca == VAZIO &&
-                    ((pecaBranca(jogo->tabuleiro[meioLinha][meioColuna].peca)) &&
-                     (pecaPreta(peca)))) {
-                    movimentosValidos[numMovimentos++] = (Movimento){linha, coluna, captLinha, captColuna};
+                    pecaBranca(jogo->tabuleiro[meioLinha][meioColuna].peca)) {
+                    
+                    Movimento m = {linha, coluna, captLinha, captColuna, ehDama, 1, 0};
+                    movimentosValidos[numMovimentos++] = m;
                 }
             }
         }
     }
 
     if (numMovimentos == 0) {
-        printf("\n IA não pode mover!\n");
-        return;
+        printf("%s (IA) nao tem movimentos validos e perde o jogo!\n", jogadorIA->nome);
+        salvarHistorico(jogo);
+        exit(0);
     }
 
-    srand(time(NULL));
-    Movimento escolhido = movimentosValidos[rand() % numMovimentos];
+    for (int i = 0; i < numMovimentos; i++) {
+        Movimento *movimento = &movimentosValidos[i];
+        if (movimento->captura)
+            movimento->pontuacao += 100;
+        if (movimento->dama)
+            movimento->pontuacao += 50;
 
-    moverPeca(jogo,
-              escolhido.linhaOrigem, escolhido.colunaOrigem,
-              escolhido.linhaDestino, escolhido.colunaDestino,
+        int vulnerabilidade = 0;
+        for (int d = 0; d < 4; d++) {
+            int dl = (d == 0 || d == 1) ? 1 : -1;
+            int dc = (d == 0 || d == 2) ? 1 : -1;
+
+            int linhaOponente = movimento->linhaDestino + dl;
+            int colunaOponente = movimento->colunaDestino + dc;
+            int linhaApos = movimento->linhaDestino + 2 * dl;
+            int colunaApos = movimento->colunaDestino + 2 * dc;
+
+            if (posicaoValida(linhaOponente, colunaOponente) && posicaoValida(linhaApos, colunaApos)) {
+                TipoPeca pecaOponente = jogo->tabuleiro[linhaOponente][colunaOponente].peca;
+                if (pecaBranca(pecaOponente) && jogo->tabuleiro[linhaApos][colunaApos].peca == VAZIO) {
+                    vulnerabilidade++;
+                }
+            }
+        }
+        if (vulnerabilidade > 0)
+            movimento->pontuacao -= vulnerabilidade * 30;
+    }
+
+    int melhorPontuacao = -9999;
+    for (int i = 0; i < numMovimentos; i++) {
+        if (movimentosValidos[i].pontuacao > melhorPontuacao) {
+            melhorPontuacao = movimentosValidos[i].pontuacao;
+        }
+    }
+
+    Movimento melhoresMovimentos[200];
+    int countMelhores = 0;
+    for (int i = 0; i < numMovimentos; i++) {
+        if (movimentosValidos[i].pontuacao == melhorPontuacao) {
+            melhoresMovimentos[countMelhores++] = movimentosValidos[i];
+        }
+    }
+
+    Movimento selecionado = melhoresMovimentos[rand() % countMelhores];
+
+    moverPeca(jogo, selecionado.linhaOrigem, selecionado.colunaOrigem,
+              selecionado.linhaDestino, selecionado.colunaDestino,
               jogadorOponente, jogadorIA);
 
-    printf("\n IA moveu de %c%d para %c%d\n",
-           'A' + escolhido.colunaOrigem, escolhido.linhaOrigem + 1,
-           'A' + escolhido.colunaDestino, escolhido.linhaDestino + 1);
+    printf("%s (IA) moveu de (%d, %d) para (%d, %d)\n", jogadorIA->nome,
+           selecionado.linhaOrigem, selecionado.colunaOrigem,
+           selecionado.linhaDestino, selecionado.colunaDestino);
+
+    if (selecionado.captura) {
+        int novaLinha = selecionado.linhaDestino;
+        int novaColuna = selecionado.colunaDestino;
+        int capturaExtra = 1;
+
+        while (capturaExtra) {
+            capturaExtra = 0;
+            for (int d = 0; d < 4; d++) {
+                int dl = (d == 0 || d == 1) ? 1 : -1;
+                int dc = (d == 0 || d == 2) ? 1 : -1;
+
+                int meioLinha = novaLinha + dl;
+                int meioColuna = novaColuna + dc;
+                int destLinha = novaLinha + 2 * dl;
+                int destColuna = novaColuna + 2 * dc;
+
+                if (posicaoValida(destLinha, destColuna) &&
+                    pecaBranca(jogo->tabuleiro[meioLinha][meioColuna].peca) &&
+                    jogo->tabuleiro[destLinha][destColuna].peca == VAZIO) {
+
+                    moverPeca(jogo, novaLinha, novaColuna, destLinha, destColuna,
+                              jogadorOponente, jogadorIA);
+                    printf("\nIA fez captura encadeada para %c%d\n",
+                           'A' + destColuna, destLinha + 1);
+                    novaLinha = destLinha;
+                    novaColuna = destColuna;
+                    capturaExtra = 1;
+                    break;
+                }
+            }
+        }
+    }
 }
 
 int verificarVencedor(Jogo *jogo, Jogador *jogadorBrancas, Jogador *jogadorPretas) {
     int temBrancas = 0, temPretas = 0;
-
     for (int i = 0; i < MAX; i++) {
         for (int j = 0; j < MAX; j++) {
             TipoPeca peca = jogo->tabuleiro[i][j].peca;
-
             if (pecaBranca(peca)) temBrancas = 1;
-            if (pecaPreta(peca)) temPretas = 1;
-        }
+            if (pecaPreta(peca)) temPretas = 1; 
+        } 
     }
-
     if (!temPretas) {
         printf("\n%s (Brancas) venceu o jogo!\n", jogadorBrancas->nome);
-        salvarHistorico(jogo);
-        return 1;
+        salvarHistorico(jogo); return 1;
     }
     else if (!temBrancas) {
         printf("\n%s (Pretas) venceu o jogo!\n", jogadorPretas->nome);
-        salvarHistorico(jogo);
-        return 1;
+        salvarHistorico(jogo); return 1;
     }
-
     return 0;
 }
